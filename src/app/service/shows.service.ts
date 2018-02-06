8644fb6690f91b8e2b9afccabbc5a7d3d2a089bb
James Kircher <g802059@ad.syniverse.com>
2018-02-06 15:49:49 -0500

    Change IShow status to slot

--- src/app/service/shows.service.ts
+++ src/app/service/shows.service.ts
@@ -2,25 +2,25 @@ import { Injectable } from '@angular/core';
 import { IShow, ITimeSlot } from '../model/shows.model';
 
 const SHOWS: IShow[] = [
-  { 'name': 'Gotham', 'network': 'FOX', 'link': 'http://www.fox.com/gotham/', 'image': 'Gotham2.gif', 'info': 'Thursdays 8:00PM (Thursday, September 21)', 'status': 'B' },
-  { 'name': 'Big Bang Theory', 'network': 'CBS', 'link': 'http://www.cbs.com/shows/big_bang_theory/', 'image': 'BigBangTheory.gif', 'info': 'Thursdays 8:00PM (Monday, September 25)', 'status': 'B' },
-  { 'name': 'Criminal Minds', 'network': 'CBS', 'link': 'http://www.cbs.com/shows/criminal_minds/', 'image': 'CriminalMinds.gif', 'info': 'Wednesdays 10:00PM (Wednesday, September 27)', 'status': 'B' },
-  { 'name': 'Supergirl', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/supergirl/', 'image': 'Supergirl.gif', 'info': 'Mondays 8:00PM (Monday, October 9)', 'status': 'B' },
-  { 'name': 'The Flash', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/the-flash/', 'image': 'TheFlash.gif', 'info': 'Tuesdays 8:00PM (Tuesday, October 10)', 'status': 'B' },
-  { 'name': 'Legends of Tomorrow', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/dcs-legends-of-tomorrow/', 'image': 'LegendsOfTomorrow.gif', 'info': 'Tuesdays 9:00PM (Tuesday, October 10)', 'status': 'B' },
-  { 'name': 'Supernatural', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/supernatural/', 'image': 'Supernatural11.gif', 'info': 'Thursdays 8:00PM (Thursday, October 12)', 'status': 'B' },
-  { 'name': 'Arrow', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/arrow/', 'image': 'Arrow.gif', 'info': 'Thursdays 9:00PM (Thursday, October 12)', 'status': 'B' },
-  { 'name': 'Star Wars Rebels', 'network': 'DisneyXD', 'link': 'http://www.starwars.com/tv-shows/star-wars-rebels', 'image': 'Rebels.gif', 'info': 'Mondays 9:00PM (Monday, October 16)', 'status': 'B' },
-  { 'name': 'The Walking Dead', 'network': 'AMC', 'link': 'http://www.amctv.com/series/The-Walking-Dead/', 'image': 'WalkingDead.gif', 'info': 'Sundays 9:00PM (Sunday, October 22)', 'status': 'B' },
-  { 'name': 'The Punisher', 'network': 'Netflix', 'link': 'http://www.netflix.com/punisher', 'image': 'Punisher.gif', 'info': 'Streaming (November 17)', 'status': 'B' },
-  { 'name': 'Vikings', 'network': 'History', 'link': 'http://www.history.com/shows/vikings', 'image': 'Vikings.gif', 'info': 'Wednesdays 9:00PM (Wednesday, November 29)', 'status': 'B' },
-  { 'name': 'The X-Files', 'network': 'FOX', 'link': 'http://www.fox.com/the-x-files/', 'image': 'XFiles2016.gif', 'info': 'Wednesdays 8:00PM (Wednesday, January 3)', 'status': 'B' },
-  { 'name': 'Homeland', 'network': 'Showtime', 'link': 'http://www.sho.com/sho/homeland/home', 'image': 'Homeland.gif', 'info': 'Sundays 9:00PM (Sunday, February 11)', 'status': 'B' },
-  { 'name': 'Ash vs Evil Dead', 'network': 'Starz', 'link': 'https://www.starz.com/series/ashvsevildead', 'image': 'AshVsEvilDead.gif', 'info': 'Sundays 9:00PM (Sunday, February 25)', 'status': 'B' },
-  { 'name': 'Jessica Jones', 'network': 'Netflix', 'link': 'http://www.netflix.com/jessicajones', 'image': 'JessicaJones.gif', 'info': 'Streaming (March 8)', 'status': 'B' },
-  { 'name': 'Krypton', 'network': 'Syfy', 'link': 'http://www.syfy.com/krypton/', 'image': 'Krypton.gif', 'info': 'Wednesdays (Wednesday, March 21)', 'status': 'B' },
-  { 'name': 'A Series of Unfortunate Events', 'network': 'Netflix', 'link': 'http://www.netflix.com/unfortunate', 'image': 'Unfortunate.gif', 'info': 'Streaming (March 30)', 'status': 'B' },
-  { 'name': 'Elementary', 'network': 'CBS', 'link': 'http://www.cbs.com/shows/elementary/', 'image': 'Elementary.gif', 'info': 'Mondays 10:00PM (Monday, April 30)', 'status': 'B' }
+  { 'name': 'Gotham', 'network': 'FOX', 'link': 'http://www.fox.com/gotham/', 'image': 'Gotham2.gif', 'info': 'Thursdays 8:00PM (Thursday, September 21)', 'slot': 'SBS' },
+  { 'name': 'Big Bang Theory', 'network': 'CBS', 'link': 'http://www.cbs.com/shows/big_bang_theory/', 'image': 'BigBangTheory.gif', 'info': 'Thursdays 8:00PM (Monday, September 25)', 'slot': 'SBS' },
+  { 'name': 'Criminal Minds', 'network': 'CBS', 'link': 'http://www.cbs.com/shows/criminal_minds/', 'image': 'CriminalMinds.gif', 'info': 'Wednesdays 10:00PM (Wednesday, September 27)', 'slot': 'SBS' },
+  { 'name': 'Supergirl', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/supergirl/', 'image': 'Supergirl.gif', 'info': 'Mondays 8:00PM (Monday, October 9)', 'slot': 'SBS' },
+  { 'name': 'The Flash', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/the-flash/', 'image': 'TheFlash.gif', 'info': 'Tuesdays 8:00PM (Tuesday, October 10)', 'slot': 'SBS' },
+  { 'name': 'Legends of Tomorrow', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/dcs-legends-of-tomorrow/', 'image': 'LegendsOfTomorrow.gif', 'info': 'Tuesdays 9:00PM (Tuesday, October 10)', 'slot': 'SBS' },
+  { 'name': 'Supernatural', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/supernatural/', 'image': 'Supernatural11.gif', 'info': 'Thursdays 8:00PM (Thursday, October 12)', 'slot': 'SBS' },
+  { 'name': 'Arrow', 'network': 'CW', 'link': 'http://www.cwtv.com/shows/arrow/', 'image': 'Arrow.gif', 'info': 'Thursdays 9:00PM (Thursday, October 12)', 'slot': 'SBS' },
+  { 'name': 'Star Wars Rebels', 'network': 'DisneyXD', 'link': 'http://www.starwars.com/tv-shows/star-wars-rebels', 'image': 'Rebels.gif', 'info': 'Mondays 9:00PM (Monday, October 16)', 'slot': 'SBS' },
+  { 'name': 'The Walking Dead', 'network': 'AMC', 'link': 'http://www.amctv.com/series/The-Walking-Dead/', 'image': 'WalkingDead.gif', 'info': 'Sundays 9:00PM (Sunday, October 22)', 'slot': 'SBS' },
+  { 'name': 'The Punisher', 'network': 'Netflix', 'link': 'http://www.netflix.com/punisher', 'image': 'Punisher.gif', 'info': 'Streaming (November 17)', 'slot': 'SBS' },
+  { 'name': 'Vikings', 'network': 'History', 'link': 'http://www.history.com/shows/vikings', 'image': 'Vikings.gif', 'info': 'Wednesdays 9:00PM (Wednesday, November 29)', 'slot': 'SBS' },
+  { 'name': 'The X-Files', 'network': 'FOX', 'link': 'http://www.fox.com/the-x-files/', 'image': 'XFiles2016.gif', 'info': 'Wednesdays 8:00PM (Wednesday, January 3)', 'slot': 'SBS' },
+  { 'name': 'Homeland', 'network': 'Showtime', 'link': 'http://www.sho.com/sho/homeland/home', 'image': 'Homeland.gif', 'info': 'Sundays 9:00PM (Sunday, February 11)', 'slot': 'SBS' },
+  { 'name': 'Ash vs Evil Dead', 'network': 'Starz', 'link': 'https://www.starz.com/series/ashvsevildead', 'image': 'AshVsEvilDead.gif', 'info': 'Sundays 9:00PM (Sunday, February 25)', 'slot': 'SBS' },
+  { 'name': 'Jessica Jones', 'network': 'Netflix', 'link': 'http://www.netflix.com/jessicajones', 'image': 'JessicaJones.gif', 'info': 'Streaming (March 8)', 'slot': 'SBS' },
+  { 'name': 'Krypton', 'network': 'Syfy', 'link': 'http://www.syfy.com/krypton/', 'image': 'Krypton.gif', 'info': 'Wednesdays (Wednesday, March 21)', 'slot': 'SBS' },
+  { 'name': 'A Series of Unfortunate Events', 'network': 'Netflix', 'link': 'http://www.netflix.com/unfortunate', 'image': 'Unfortunate.gif', 'info': 'Streaming (March 30)', 'slot': 'SBS' },
+  { 'name': 'Elementary', 'network': 'CBS', 'link': 'http://www.cbs.com/shows/elementary/', 'image': 'Elementary.gif', 'info': 'Mondays 10:00PM (Monday, April 30)', 'slot': 'SBS' }
 ];
 
 const TIMESLOTS: ITimeSlot[] = [
