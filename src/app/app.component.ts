674be4dce7afe5f667adaa923ee7669eb3864c4a
Jim <James.C.KIrcher@gmail.com>
2019-02-05 21:22:59 -0500

    Update styles and responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-    .app {
-      width: 100%;
-      height: 100%;
-      background-color: #eee;
-    }
-  `]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
